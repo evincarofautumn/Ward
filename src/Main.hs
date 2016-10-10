b9b2c33ec7bda6c57a84eb327607dca9b7d97eef
Jon Purdy <evincarofautumn@gmail.com>
2016-10-10 15:04:55 -0700

    Parse and pretty-print translation unit.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,28 @@
 module Main where
 
+import Language.C (parseCFile)
+import Language.C.Pretty (pretty)
+import Language.C.System.GCC (newGCC)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+import Text.PrettyPrint (render)
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+  let temporaryDirectory = Nothing
+  let preprocessor = newGCC "gcc"
+  args <- getArgs
+  (filePath, preprocessorFlags) <- case args of
+    path : flags -> return (path, flags)
+    [] -> do
+      hPutStrLn stderr "Usage: ward <path> <flags>"
+      exitFailure
+  result <- parseCFile preprocessor temporaryDirectory preprocessorFlags filePath
+  case result of
+    Left parseError -> do
+      putStrLn "Parse error:"
+      print parseError
+    Right translationUnit -> do
+      putStrLn "Translation unit:"
+      putStrLn $ render $ pretty translationUnit
