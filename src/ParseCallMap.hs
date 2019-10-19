29e23c5c8e085aa6555a9ad8bb66ed5338048a92
Ben Gamari <ben@smart-cactus.org>
2019-10-19 17:49:29 -0400

    Show filename in error

--- src/ParseCallMap.hs
+++ src/ParseCallMap.hs
@@ -1,13 +1,15 @@
 module ParseCallMap (fromFile, fromSource) where
 
 import Types
+import Data.Bifunctor
 import Data.Aeson
 import qualified Data.ByteString.Lazy as BSL
 
 -- | Parse the given callmap graph file.  Assumes UTF8 encoding (will use
 -- U+FFFD replacement character on UTF8 decoding errors).
 fromFile :: FilePath -> IO (Either String CallMap)
-fromFile path = fromSource <$> BSL.readFile path
+fromFile path = first fixError . fromSource <$> BSL.readFile path
+  where fixError err = path ++ ": " ++ err
 
 -- | @fromSource path txt@ parses @txt@ and returns the resulting 'CallMap'.
 -- (The @path@ is used in error messages)
